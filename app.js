346df31a3ba5dbee6e5ac148d79a393fa182b1d9
Ony3dika <akpa3220@gmail.com>
2025-07-23 13:34:55 +0100

    [FIXED] cors added to support local host for testing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 const app = express();
+// Or for specific origin:
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // if using cookies or sessions
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
